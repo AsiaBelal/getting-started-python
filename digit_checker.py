f2f8a4287c8c6539cf6f2c9709ce255e1696e71b
Asia Said Belal <belalasia36@gmail.com>
2025-10-04 22:00:10 +0300

    Create digit_checker.py

---
+++ digit_checker.py
@@ -0,0 +1,12 @@
+num = int(input("please enter a num: "))
+
+if num < 10:
+    print("The number is less than 4 digits.")
+elif num < 100:
+    print("The number is less than 4 digits.")
+elif num < 1000:
+    print("The number is less than 4 digits.")
+elif num < 10000:
+    print("The number is 4 digits.")
+else:
+    print("The number is more than 4 digits.")
