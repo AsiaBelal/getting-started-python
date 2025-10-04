36af4b1672a7756105ec6b26e37599381b1fe144
Asia Said Belal <belalasia36@gmail.com>
2025-10-04 21:38:16 +0300

    Create variable-input0.py

---
+++ variable-input0.py
@@ -0,0 +1,13 @@
+# define the variables
+x = int(input("enter the first num: "))
+y = int(input("enter the second num: "))
+z = int(input("enter the third num: "))
+
+numbers = [x, y, z]
+maximum = max(numbers)
+
+# print the numbers that we will enter
+print("enter number one:", x)
+print("enter number two:", y)
+print("enter number three:", z)
+print("maxnum is:", maximum)
