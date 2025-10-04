361eb07b5ac7737cc3439e72b58d852c5ffd2c4b
Asia Said Belal <belalasia36@gmail.com>
2025-10-04 21:39:03 +0300

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,11 @@
+# set variables
+A = 50
+B = 2
+
+# Arithmetic operations
+print("addition: ", (A + B))
+print("subtracting: ", (A - B))
+print("multiplying: ", (A * B))
+print("A to the power B: ", (A ** B))
+print("division: ", (A / B))
+print("A divided by B remainder: ", (A % B))
