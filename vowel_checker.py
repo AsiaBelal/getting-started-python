fa5fb7ca8772a0750addb6ca3edc86328d700435
Asia Said Belal <belalasia36@gmail.com>
2025-10-04 21:57:34 +0300

    Create vowel_checker.py

---
+++ vowel_checker.py
@@ -0,0 +1,58 @@
+# Get input from the user
+character = str(input("Please enter the character: "))
+
+# Check if the character is a vowel or not
+if character == 'a':
+    print("Vowel")
+elif character == 'b':
+    print("Not vowel")
+elif character == 'c':
+    print("Not vowel")
+elif character == 'd':
+    print("Not vowel")
+elif character == 'e':
+    print("Vowel")
+elif character == 'f':
+    print("Not vowel")
+elif character == 'g':
+    print("Not vowel")
+elif character == 'h':
+    print("Not vowel")
+elif character == 'i':
+    print("Vowel")
+elif character == 'j':
+    print("Not vowel")
+elif character == 'k':
+    print("Not vowel")
+elif character == 'l':
+    print("Not vowel")
+elif character == 'm':
+    print("Not vowel")
+elif character == 'n':
+    print("Not vowel")
+elif character == 'o':
+    print("Vowel")
+elif character == 'p':
+    print("Not vowel")
+elif character == 'q':
+    print("Not vowel")
+elif character == 'r':
+    print("Not vowel")
+elif character == 's':
+    print("Not vowel")
+elif character == 't':
+    print("Not vowel")
+elif character == 'u':
+    print("Vowel")
+elif character == 'v':
+    print("Not vowel")
+elif character == 'w':
+    print("Not vowel")
+elif character == 'x':
+    print("Not vowel")
+elif character == 'y':
+    print("Not vowel")
+elif character == 'z':
+    print("Not vowel")
+else:
+    print("Error: Please enter alphabetic character.")
